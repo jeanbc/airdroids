948b2b20280348e8ac40cdcb071a45b8a34526d9
root <root@ip-172-31-18-131.ec2.internal>
2014-04-12 04:07:45 -0400

    init commit

---
+++ plugin/contact-form/config.php
@@ -0,0 +1,20 @@
+<?php
+		define('CONTACT_FORM_TO_NAME','AirDroids Support');
+		// define('CONTACT_FORM_TO_EMAIL','croth@airdroids.com,treuter@airdroids.com,tj@airdroids.com');
+		define('CONTACT_FORM_TO_EMAIL','team@airdroids.com');
+
+		define('CONTACT_FORM_SMTP_HOST','');
+		define('CONTACT_FORM_SMTP_USER','');
+		define('CONTACT_FORM_SMTP_PORT','');
+		define('CONTACT_FORM_SMTP_SECURE','');
+		define('CONTACT_FORM_SMTP_PASSWORD','');
+
+		define('CONTACT_FORM_SUBJECT','AirDroids Landing Page: Contact from WWW');
+
+		define('CONTACT_FORM_MSG_INVALID_DATA_NAME','Please enter your name.');
+		define('CONTACT_FORM_MSG_INVALID_DATA_MAIL','Please enter valid e-mail.');
+		define('CONTACT_FORM_MSG_INVALID_DATA_MESSAGE','Please enter your message.');
+
+		define('CONTACT_FORM_SEND_MSG_OK','Thank you for contacting us.');
+		define('CONTACT_FORM_SEND_MSG_ERROR','Sorry, we can\'t send this message.');
+?>
